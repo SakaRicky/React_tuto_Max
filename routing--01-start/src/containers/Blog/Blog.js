7dcf6d35b7a89b7ca25083125b766464ca03c2ea
Saka Ricky <sakaricky91@gmail.com>
2020-12-17 13:36:59 +0100

    Used lazy loaders and finished with the SPA part of the course

--- routing--01-start/src/containers/Blog/Blog.js
+++ routing--01-start/src/containers/Blog/Blog.js
@@ -3,16 +3,19 @@ import React, { Component } from 'react';
 // import axios from '../../axios';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
+import './Blog.css'
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+// import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
 
-
-import './Blog.css';
+const asyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+})
 
 class Blog extends Component {
 
     state = {
-        auth: false
+        auth: true
     }
 
     render () {
@@ -37,7 +40,7 @@ class Blog extends Component {
                 {/* <Route path="/" render={() => <h1>Home</h1>} />
                 <Route path="/" exact render={() => <h1>Home2</h1>} /> */}
                 <Switch>
-                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null }
+                    {this.state.auth ? <Route path="/new-post" component={asyncNewPost} /> : null }
                     <Route path="/posts" component={Posts} />
                     <Route render={() => <h1>Not Found</h1>} />
                     {/* <Redirect from='/' to='/posts' /> */}
